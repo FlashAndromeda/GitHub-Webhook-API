ae0433675cf3141f74f899b5fa7b811d465ed3bc
Michał Fleites-Jończyk <michalfleites@gmail.com>
2023-04-01 00:42:29 +0200

    Bugfixing (hopefully)

--- main.py
+++ main.py
@@ -6,9 +6,6 @@ from subprocess import call
 
 app = Flask(__name__)
 
-webhook_key = environ.get('WEBHOOK_KEY')
-
-
 @app.route('/', methods=['GET'])
 def index():
 	return "Nothing to see here!", 200
@@ -16,27 +13,29 @@ def index():
 
 @app.route('/gh_webhook', methods=['POST'])
 def webhook():
+	webhook_key = environ.get('WEBHOOK_KEY')
+
 	if not request.headers['x-hub-signature-256']:
 		return "Missing X-Hub-Signature-256!", 500
 
-	if not validate_signature():
+	if not validate_signature(webhook_key):
 		return "Bad Signature!", 500
 
-	branch = json.loads((request.data).decode("utf-8")).get('ref').split('/')[-1]
+	# branch = json.loads((request.data).decode("utf-8")).get('ref').split('/')[-1]
+	#
+	# if branch == 'dev':
+	# 	call('(cd /home/cgi/www-test/ && git pull origin dev)')
+	# 	return "Updated dev branch in '/home/cgi/www-test/'", 200
+	#
+	# elif branch == 'main':
+	# 	call('cd /home/cgi/www/ && git pull origin main')
+	# 	return "Updated main branch in '/home/cgi/www/'", 200
+	#
+	# else:
+	# 	return f"Cannot find local branch corresponding to {branch} :(", 404
 
-	if branch == 'dev':
-		call('(cd /home/cgi/www-test/ && git pull origin dev)')
-		return "Updated dev branch in '/home/cgi/www-test/'", 200
 
-	elif branch == 'main':
-		call('cd /home/cgi/www/ && git pull origin main')
-		return "Updated main branch in '/home/cgi/www/'", 200
-
-	else:
-		return f"Cannot find local branch corresponding to {branch} :(", 404
-
-
-def validate_signature():
+def validate_signature(webhook_key):
 	key = bytes(webhook_key, 'utf-8')
 
 	expected_signature = new(key=key, msg=request.data,digestmod=sha256).hexdigest()
